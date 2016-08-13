006dc4bf0ac65e04bce4ca514cad8a304af05647
jkredzvr <jchin.bu@gmail.com>
2016-08-13 12:45:02 -0600

    Editor files for TUtorail 5 Splines

---
+++ Assets/Editor/BezierSplineInspector.cs
@@ -0,0 +1,173 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BezierSpline))]
+public class BezierSplineInspector : Editor
+{
+    //# steps between points for showing velocity
+    private const int stepsPerCurve = 10;
+    private const float directionScale = 0.5f;
+    private const float handleSize = .04f;
+    private const float pickSize = .06f;
+
+    private static Color[] modeColors = {
+        Color.white,
+        Color.yellow,
+        Color.cyan
+    };
+
+    private BezierSpline spline;
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+    private int selectedIndex = -1;
+
+    //Handles whats seen in the inspector
+    public override void OnInspectorGUI() {
+        //keep drawing the default inspector
+        //DrawDefaultInspector();
+        spline = target as BezierSpline;
+        EditorGUI.BeginChangeCheck();
+        bool loop = EditorGUILayout.Toggle("Loop", spline.Loop);
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(spline, "Toggle Loop");
+            EditorUtility.SetDirty(spline);
+            spline.Loop = loop;
+        }
+        if (selectedIndex >= 0 && selectedIndex < spline.ControlPointCount)
+        {
+            DrawSelectedPointInspector();
+        }
+        if (GUILayout.Button("Add Curve")) {
+            Undo.RecordObject(spline, "Add Curve");
+            spline.AddCurve();
+            EditorUtility.SetDirty(spline);
+        }
+    }
+
+    private void DrawSelectedPointInspector() {
+        GUILayout.Label("Selected Point");
+        EditorGUI.BeginChangeCheck();
+        Vector3 point = EditorGUILayout.Vector3Field("Position", spline.GetControlPoint(selectedIndex));
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(spline, "Move Point");
+            EditorUtility.SetDirty(spline);
+            spline.SetControlPoint(selectedIndex, point);
+        }
+        EditorGUI.BeginChangeCheck();
+        BezierControlPointMode mode = (BezierControlPointMode)
+            EditorGUILayout.EnumPopup("Mode", spline.GetControlPointMode(selectedIndex));
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(spline, "Change Point Mode");
+            spline.SetControlPointMode(selectedIndex, mode);
+            EditorUtility.SetDirty(spline);
+        }
+    }
+
+    //Handles whats seen in the scene
+    private void OnSceneGUI()
+    {
+        spline = target as BezierSpline;
+        handleTransform = spline.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+
+        //Initialize the points
+        Vector3 p0 = ShowPoint(0);
+
+        //Loop through bezier splines
+        for (int i = 1; i < spline.ControlPointCount; i += 3)
+        {
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i+1);
+            Vector3 p3 = ShowPoint(i+2);
+
+            //Draw lines between points
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
+
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
+            //reset end point of spline to be new start of next spline
+            p0 = p3;
+        }
+        ShowDirections();     
+        //Old method of drawing tangent lines between two points
+        //Handles.color = Color.white;
+        //Vector3 lineStart = curve.GetPoint(0f);
+        //Handles.color = Color.green;
+        //Handles.DrawLine(lineStart, lineStart + curve.GetDirection(0F));
+        //for (int i = 1; i <= lineSteps; i++) {
+        //    //Draw # of lineSteps tangent lines between two points
+        //    //lineend is the interpolated point between p0 and p1 in lineSteps
+        //    Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
+        //    Handles.color = Color.white;
+        //    Handles.DrawLine(lineStart, lineEnd);
+        //    Handles.color = Color.green;
+        //    Handles.DrawLine(lineEnd, lineEnd + curve.GetDirection(i / (float)lineSteps));
+        //    lineStart = lineEnd;
+        //}
+    }
+
+   
+
+    //Crowded with all those transform handles. We could only show a handle for the active point. Then then other points can suffice with dots.
+    private Vector3 ShowPoint(int index) {
+        Vector3 point = handleTransform.TransformPoint(spline.GetControlPoint(index));
+        
+        //This fixes the size of the handles so it can be scaled with zooming in and out.
+        float size = HandleUtility.GetHandleSize(point);
+
+        if (index == 0) {
+            size *= 2f;
+        }
+
+        //handle becomes white dot
+        Handles.color = modeColors[(int)spline.GetControlPointMode(index)];
+        //if the current point is the selected handle
+        if (Handles.Button(point, handleRotation, size*handleSize, size*pickSize, Handles.DotCap))
+        {
+            selectedIndex = index;
+            Repaint();
+        }
+        if (selectedIndex == index)
+        {
+            EditorGUI.BeginChangeCheck();
+            point = Handles.DoPositionHandle(point, handleRotation);
+            if (EditorGUI.EndChangeCheck()) {
+                Undo.RecordObject(spline, "Move Point");
+                EditorUtility.SetDirty(spline);
+                spline.SetControlPoint(index,handleTransform.InverseTransformPoint(point));
+            }
+        }
+        
+        return point;
+    }
+
+    //Old Show point that made all handles into lines
+    //private Vector3 ShowPoint(int index)
+    //{
+    //    Vector3 point = handleTransform.TransformPoint(spline.points[index]);
+    //    EditorGUI.BeginChangeCheck();
+    //    point = Handles.DoPositionHandle(point, handleRotation);
+    //    if (EditorGUI.EndChangeCheck())
+    //    {
+    //        Undo.RecordObject(spline, "Move Point");
+    //        EditorUtility.SetDirty(spline);
+    //        spline.points[index] = handleTransform.InverseTransformPoint(point);
+    //    }
+    //    return point;
+    //}
+
+    private void ShowDirections() {
+        Handles.color = Color.green;
+        Vector3 point = spline.GetPoint(0f);
+        Handles.DrawLine(point, point + spline.GetDirection(0f) * directionScale);
+        int steps = stepsPerCurve*spline.CurveCount;
+        for (int i = 1; i <= steps; i++) {
+            point = spline.GetPoint(i / (float)steps);
+            Handles.DrawLine(point, point + spline.GetDirection(i / (float)steps) * directionScale);
+        }
+    }
+
+    
+}
---
+++ Assets/Editor/LineInspector.cs
@@ -0,0 +1,64 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+//Lets unity know to use our class instead of 
+//the default editor for Line components.
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+
+
+    private void OnSceneGUI()
+    {
+
+        //target is a special variable set to object being drawn when OnSceneGUI is called
+        Line line = target as Line;
+
+        //Handles operates in the world space, while points are in the local space.
+        //Must explicitly convert the points into world space, otherwise transform changes on the 
+        //points (scale, move, rotate), wont affect the line
+
+        //Initial Code - Handles takes the points vector and plots it directly to the WorldSpace.  Does not take into account the Line gameobject's position relative to 
+        //World space. 
+        //Handles.color = Color.white;
+        //will plot directly to world space
+        //Handles.DrawLine(line.p0, line.p1);
+
+
+        //create a handle transform using the line's global transform position
+        Transform handleTransform = line.transform;
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+
+        ////Take the local positions of p0 and p1 relative to the line gameobject and transform it to global coordinates
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+
+        Handles.color = Color.white;
+        //plot the positions of the point in the global space
+        Handles.DrawLine(p0, p1);
+
+        //Updates the line points to be moved in the editor
+        EditorGUI.BeginChangeCheck();
+        p0 = Handles.DoPositionHandle(p0, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            //Allows for any chnages to be undone
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+
+            //changes can be undone
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+    }
+}
